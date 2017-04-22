fb85976aa05fd5e25c836bd8241d22dcd848228d
Jiwan <jiwanaust123@gmail.com>
2017-04-22 20:56:14 +1000

    First Push

---
+++ app/src/main/java/com/designsquad/myfinder/MapsActivity.java
@@ -0,0 +1,141 @@
+package com.designsquad.myfinder;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.provider.SyncStateContract;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.text.TextUtils;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private static final long LOCATION_REFRESH_TIME = 100;
+    private static final float LOCATION_REFRESH_DISTANCE = 100;
+    private GoogleMap mMap;
+    private LocationManager mLocationManager;
+    Geocoder geocoder = null;
+
+    private final LocationListener mLocationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(final Location location) {
+            Toast.makeText(MapsActivity.this,"Location found",Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+            Toast.makeText(MapsActivity.this,"Location enable",Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        geocoder = new Geocoder(this, Locale.getDefault());
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            return;
+        }
+
+        mMap.setMyLocationEnabled(true);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            return;
+        }
+        mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, LOCATION_REFRESH_TIME,
+                LOCATION_REFRESH_DISTANCE, mLocationListener);
+
+        LocationManager locationManager = (LocationManager)getSystemService
+                (Context.LOCATION_SERVICE);
+        Location getLastLocation = locationManager.getLastKnownLocation
+                (LocationManager.PASSIVE_PROVIDER);
+        double currentLongitude = getLastLocation.getLongitude();
+        double currentLatitude = getLastLocation.getLatitude();
+
+        LatLng currentLocation = new LatLng(currentLatitude, currentLongitude);
+
+        //Toast.makeText(MapsActivity.this,"Location is"+currentLatitude+" "+currentLongitude,Toast.LENGTH_LONG).show();
+
+        List<Address> addresses = null;
+
+        try {
+            addresses = geocoder.getFromLocation(
+                    currentLatitude,
+                    currentLongitude,
+                    // In this sample, get just a single address.
+                    1);
+        } catch (IOException ioException) {
+        } catch (IllegalArgumentException illegalArgumentException) {
+        }
+
+        String txtAddress = "";
+
+        // Handle case where no address was found.
+        if (addresses == null || addresses.size()  == 0) {
+            txtAddress = "Not found";
+        } else {
+            Address address = addresses.get(0);
+            ArrayList<String> addressFragments = new ArrayList<String>();
+
+            for(int i = 0; i <= address.getMaxAddressLineIndex(); i++) {
+                addressFragments.add(address.getAddressLine(i));
+            }
+
+            txtAddress = TextUtils.join(System.getProperty("line.separator"),
+                            addressFragments);
+        }
+
+        googleMap.addMarker(new MarkerOptions()
+                .position(new LatLng(currentLatitude, currentLongitude))
+                .title(txtAddress));
+
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(currentLatitude, currentLongitude), 14));
+
+    }
+
+}
